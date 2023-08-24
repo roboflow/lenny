514d1b4a13d7f5c9acb9d90aa41deaab94691efb
Ariuntuya Altanzaya <60797147+artyaltanzaya@users.noreply.github.com>
2023-08-23 22:17:42 -0400

    resolved readME, csv conflict

--- src/components/kapa.js
+++ src/components/kapa.js
@@ -7,6 +7,7 @@ const App = () => {
   const [error, setError] = useState(null);
   const [query, setQuery] = useState('');
   const [loading, setLoading] = useState(false);
+  const apiKey = process.env.NEXT_PUBLIC_KAPA_KEY;
 
   const sendIcon = (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -19,7 +20,8 @@ const App = () => {
         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
     </svg>
   )
-
+ 
+  //---------- add example prompts here
   const prompts = [
     "What is object detection?",
     "What is instance segmentation?"
@@ -80,7 +82,7 @@ const App = () => {
       const response = await fetch(`https://api.kapa.ai/query/v1/stream?query=${encodeURIComponent(query)}`, {
         method: 'GET',
         headers: {
-          "X-API-TOKEN": "70baabfa-0af7-4818-8214-cf39691def7f",
+          "X-API-TOKEN": apiKey,
         }
       });
       
