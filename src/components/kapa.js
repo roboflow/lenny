245d74b4ef140a4edb2c5c3c0162041580306c85
James Gallagher <jamesg@jamesg.blog>
2023-09-04 23:39:57 +0100

    update code

--- src/components/kapa.js
+++ src/components/kapa.js
@@ -1,29 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
+import Script from 'next/script';
 
 const App = () => {
   const [answer, setAnswer] = useState('');
   const [error, setError] = useState(null);
   const [query, setQuery] = useState('');
   const [loading, setLoading] = useState(false);
+  const [hasChosenPrompt, setHasChosenPrompt] = useState(false);
   const apiKey = process.env.NEXT_PUBLIC_KAPA_KEY;
 
   const sendIcon = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
       <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
     </svg>
   )
 
   //---------- add example prompts here
   const prompts = [
-    "What is object detection?",
-    "What is instance segmentation?",
-    "Does YOLOv8 object detection take polygon annotations?",
-    "Does Roboflow iOS deployment support classification models?",
-    "What is the difference between instance segmentation and semantic segmentation?",
-    "What is the difference between polygon vs bounding box?",
-    "How does YOLOv8 work?"
-  ];
+    "What are the best practices for labeling data?",
+    "How do I filter detection results with supervision?",
+    "When should I use object detection vs instance segmentation?",
+    "How do I use autodistill to autolabel images?",
+    "How do I deploy to an NVIDIA Jetson?",
+    "What is CLIP?",
+    "Write code to count the number of objects my model sees from its predictions.",
+    "Where is my Roboflow API key?",
+    "When should I use the cloud API vs deploy to the edge?",
+    "What augmentations are best for aerial images?",
+    "How do I detect small objects?",
+    "How do I use Segment Anything to label images?"
+];
   
 
   const process_stream = async (response) => {
@@ -73,6 +80,8 @@ const App = () => {
 
   const fetchData = async () => {
     setLoading(true);
+    setHasChosenPrompt(true);
+
     try {
       const response = await fetch(`https://api.kapa.ai/query/v1/stream?query=${encodeURIComponent(query)}`, {
         method: 'GET',
@@ -91,7 +100,7 @@ const App = () => {
       setError(`Request failed: ${error.message}`);
     }
     setLoading(false);
-    setQuery('')
+    // setQuery('')
   };
 
   const handleQuerySubmit = () => {
@@ -100,66 +109,51 @@ const App = () => {
     fetchData();
   };
 
-
   return (
-        <body>
-            <header>
-                <div id="hero">
-                    <img src="https://roboflow-chat.vercel.app/lenny.svg" height="50" width="50" />
-                    <h1>Ask Lenny about your computer vision project.</h1>
-                    <p>Lenny is the power of GPT-4, plus <span style="color: #986bed;">500+ blog posts</span>, <span style="color: rgba(0,255,206);">100+ docs pages</span>, and Roboflow developer documentation.</p>
-                </div>
-            </header>
-            <main>
-                <ul>
-                    <li><a href="#">What is object detection?</a></li>
-                    <li><a href="#">Lenny!</a></li>
-                    <li><a href="#">Does YOLOv8 object detection take polygon annotations?</a></li>
-                    <li><a href="#">Does Roboflow iOS deployment support classification models?</a></li>
-                    <li><a href="#">Instance vs semantic segmentation</a></li>
-                    <li><a href="#">Polygon vs bounding box</a></li>
-                    <li><a href="#">How does YOLOv8 work?</a></li>
-                    <li><a href="#">What is object detection?</a></li>
-                    <li><a href="#">What is object detection?</a></li>
-                    <li><a href="#">How does YOLOv8 work?</a></li>
-                    <li><a href="#">What is object detection?</a></li>
-                    <li><a href="#">What is object detection?</a></li>
-                </ul>
-                <section class="overlay">
-                    <div class="overlay-content">
-                        <h1>Lenny</h1>
-                        <p style="margin-bottom: 20px;">Find answers to questions about Roboflow and computer vision.</p>
-                        <div id="chat">
-                            <dl>
-                                <dt>
-                                    <p>What is object detection?</p>
-                                </dt>
-                                <dd>
-                                    <p>Object detection is the task of identifying objects in an image and drawing a bounding box around them.</p>
-                                </dd>
-                            </dl>
-                            <dl>
-                                <dt>
-                                    <p>What is object detection?</p>
-                                </dt>
-                                <dd>
-                                    <p>Object detection is the task of identifying objects in an image and drawing a bounding box around them.</p>
-                                </dd>
-                            </dl>
-                        </div>
+    <div>
+      <Script src="/script.js" />
+        <header>
+            <div id="hero">
+                <img src="https://roboflow-chat.vercel.app/lenny.svg" height="50" width="50" />
+                <h1>Ask Lenny about your computer vision project.</h1>
+                <p>Lenny is the power of GPT-4, plus <span className="callout-color-purple">500+ blog posts</span>, <span className="callout-color-blue">100+ docs pages</span>, and Roboflow developer documentation.</p>
+            </div>
+        </header>
+        <main>
+            <ul id="prompts">
+                {prompts.map((prompt, index) => (
+                    <li key={index} className={ index > 4 ? 'hide-on-mobile' : '' }><a href="#" onClick={() => { setQuery(prompt); }}>{prompt}</a></li>
+                ))}
+            </ul>
+            {(hasChosenPrompt || answer) && (
+              <section className="overlay">
+                  <div className="overlay-content">
+                  {loading && (
+                      <div id="hero">
+                          <img src="https://roboflow-chat.vercel.app/lenny.svg" height="50" width="50" />
+                          <h1>Gathering sources to answer your question<span id="ellipsis">.</span></h1>
+                      </div>
+                  )}
+                  {answer && (
+                    <div>
+                      <h2>{query}</h2>
+                      <ReactMarkdown allowDangerousHtml>{answer}</ReactMarkdown>
                     </div>
+                  )}
+                  </div>
                 </section>
-                <footer>
-                    <div class="footer-wrapper">
-                        <form action="https://roboflow-chat.vercel.app/">
-                            <input type="text" placeholder="Ask a question..." />
-                            <p style="color: f7f7f7; font-size: 0.7em; text-align: center;;">Note: Lenny, powered by ChatGPT, is a beta product. It may produce inaccurate information.</p>
-                        </form>
-                        <svg height="50" style="margin-bottom: 15px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"></path></svg>
-                    </div>
-                </footer>
-            </main>
-        </body>
+            )}
+            <footer>
+                <div className="footer-wrapper">
+                    <form action="https://roboflow-chat.vercel.app/" onSubmit={(e) => { e.preventDefault(); handleQuerySubmit(); }}>
+                        <input type="text" placeholder="Ask a question..." value={query} onChange={(e) => setQuery(e.target.value)} autoFocus />
+                        <p class="disclaimer">Note: Lenny, powered by ChatGPT, is a beta product. It may produce inaccurate information.</p>
+                    </form>
+                    <svg height="50"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"></path></svg>
+                </div>
+            </footer>
+        </main>
+      </div>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Kapa from '../components/kapa';
+import Head from 'next/head';
 
 const HomePage = () => {
   return (
     <div>
+      <Head>
+        <meta charset="utf-8" />
+        
+        <title>Lenny: Computer vision help, powered by GPT-4</title>
+        <meta name="description" content="Lenny is the power of GPT-4, plus 500+ blog posts, 100+ docs pages, and Roboflow developer documentation." />
+
+        <meta content="https://assets-global.website-files.com/5f6bc60e665f54545a1e52a5/642746dba53a59a614a64b35_roboflow-open-graph.png" property="og:image" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        
+        <link rel="icon" href="/lenny.svg" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <Kapa />
     </div>
   );
